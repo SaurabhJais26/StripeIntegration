fb9d6a2433758ce2b03b5e9493d21baf829c2698
Saurabh Jaiswal <saurabh@astrosage.com>
2025-12-03 23:32:45 +0530

    Initial Commit

---
+++ StripeIntegration/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StripeIntegration
+//
+//  Created by Saurabh Jaiswal on 03/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StripeIntegration/StripeIntegrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  StripeIntegrationApp.swift
+//  StripeIntegration
+//
+//  Created by Saurabh Jaiswal on 03/12/25.
+//
+
+import SwiftUI
+
+@main
+struct StripeIntegrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
